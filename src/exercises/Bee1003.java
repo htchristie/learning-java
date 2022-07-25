5aea85afb2400d52427eb59716b9dc821f23f1b9
Cristyane Tamioso <christiehespanha@gmail.com>
2022-07-25 12:31:21 -0300

    add beecrowd exercises 1002, 1003, 1007, 1008, 1010, 1012

---
+++ src/exercises/Bee1003.java
@@ -0,0 +1,20 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Bee1003 {
+    public static void main(String[] args) {
+
+        /* Read two integer values, in this case, the variables A and B. After this, calculate the sum
+        between them and assign it to the variable SOMA. Write the value of this variable. */
+
+        Scanner scan = new Scanner(System.in);
+
+        int A, B, SOMA;
+
+        A = scan.nextInt();
+        B = scan.nextInt();
+        SOMA = A + B;
+        System.out.println("SOMA = " + SOMA);
+    }
+}

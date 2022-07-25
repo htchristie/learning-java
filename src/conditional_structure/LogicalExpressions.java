dd1f8e246b56bcf479d4f2e52f2f8fb98c9e168b
Cristyane Tamioso <christiehespanha@gmail.com>
2022-07-25 13:16:01 -0300

    add 'logical expressions' file

---
+++ src/conditional_structure/LogicalExpressions.java
@@ -0,0 +1,12 @@
+package conditional_structure;
+
+public class LogicalExpressions {
+    public static void main(String[] args) {
+
+        int x = 11, y = 3, z = 6;
+
+        System.out.println(x > y && y > z); //false
+        System.out.println(y < z || x == y); //true
+        System.out.println(!(x > y)); //false
+    }
+}

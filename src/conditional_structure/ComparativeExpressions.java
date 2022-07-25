df8e24e261fddccfa9901a75946337ab0c92b6f1
Cristyane Tamioso <christiehespanha@gmail.com>
2022-07-25 13:09:48 -0300

    add 'comparative expressions' file

---
+++ src/conditional_structure/ComparativeExpressions.java
@@ -0,0 +1,13 @@
+package conditional_structure;
+
+public class ComparativeExpressions {
+    public static void main(String[] args) {
+
+        int x = 5, y = 10;
+
+        System.out.println(y > x); //true
+        System.out.println(x == y); //false
+        System.out.println(x != y); //true
+        System.out.println(y <= x); //false
+    }
+}

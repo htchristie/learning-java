b41301f694935152fb5cb39a421b93cbab48b187
Cristyane Tamioso <christiehespanha@gmail.com>
2022-07-29 13:04:16 -0300

    add beecrowd exercises 1037, 1038, 1041, 1044, 1046, and 1051

---
+++ src/exercises/Bee1044.java
@@ -0,0 +1,34 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Bee1044 {
+    public static void main(String[] args) {
+
+        /*
+        Read two integer values (A and B). After, the program should print the message "Sao Multiplos"
+        (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
+
+        Input
+        The input has two integer numbers.
+
+        Output
+        Print the relative message to the input as stated above.
+        */
+
+        Scanner scan = new Scanner(System.in);
+
+        int A, B;
+
+        A = scan.nextInt();
+        B = scan.nextInt();
+
+        if (A%B == 0 || B%A == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+        scan.close();
+    }
+}

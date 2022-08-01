58f0351d0d7fdade31c9fd29f7cba1c6b2b69280
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-01 13:12:45 -0300

    add 'while' file

---
+++ src/repetitive_structure/While.java
@@ -0,0 +1,25 @@
+package repetitive_structure;
+
+import java.util.Scanner;
+
+public class While {
+    public static void main (String [] args) {
+
+        // should be used when you don't previously know how many repetitions are needed
+
+        // Write a program which repeatedly reads numbers until the user enters "0".
+        // Once "0" is entered, print out the total.
+
+        Scanner scan = new Scanner(System.in);
+        int number, sum = 0;
+        number = scan.nextInt();
+
+        while (number != 0) {
+            sum += number;
+            number = scan.nextInt();
+        }
+
+        System.out.println("Total: " + sum);
+        scan.close();
+    }
+}

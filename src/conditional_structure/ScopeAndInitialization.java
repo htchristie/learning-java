6fda13e2f6edbbf70402453489d65a5aca38c30f
Cristyane Tamioso <christiehespanha@gmail.com>
2022-07-31 21:11:50 -0300

    comment "scope and initialization" to remove error warning

--- src/conditional_structure/ScopeAndInitialization.java
+++ src/conditional_structure/ScopeAndInitialization.java
@@ -10,15 +10,15 @@ public class ScopeAndInitialization {
         // scope: where the variable is valid, as in: where it can be referenced
         // variables cannot be used unless they've been initialized
 
-        Locale.setDefault(Locale.US);
+        //Locale.setDefault(Locale.US);
 
-        double price;
-        System.out.println(price); // error: variable 'price' might not have been initialized
+        //double price;
+        //System.out.println(price); // error: variable 'price' might not have been initialized
 
-        if (price < 200.00) {
-            double discount = price * 0.1;
-        }
+        //if (price < 200.00) {
+            //double discount = price * 0.1;
+        //}
 
-        System.out.println(discount); // error: bring 'double discount' into scope
+        //System.out.println(discount); // error: bring 'double discount' into scope
     }
 }

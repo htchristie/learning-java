3867b7cf3fe4364326818be7529fb21b5b948aaf
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-01 18:48:45 -0300

    add "for" file

---
+++ src/repetitive_structure/For.java
@@ -0,0 +1,24 @@
+package repetitive_structure;
+
+import java.util.Scanner;
+
+public class For {
+    public static void main (String [] args) {
+
+        // should be used when you previously know how many repetitions it will take
+
+        Scanner scan = new Scanner(System.in);
+
+        int sum = 0;
+        int count = scan.nextInt();
+
+        for (int i = 0; i < count; i++) {
+            int aux = scan.nextInt();
+            sum += aux;
+        }
+
+        System.out.println("Sum: " + sum);
+
+        scan.close();
+    }
+}

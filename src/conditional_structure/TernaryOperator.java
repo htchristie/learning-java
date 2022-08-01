993f88dde26ca9ca90daf3ae6649e8e1d88b13af
Cristyane Tamioso <christiehespanha@gmail.com>
2022-07-31 21:00:40 -0300

    add "ternary operator" file

---
+++ src/conditional_structure/TernaryOperator.java
@@ -0,0 +1,16 @@
+package conditional_structure;
+
+import java.util.Scanner;
+
+public class TernaryOperator {
+    public static void main (String [] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int number = scan.nextInt();
+        String evenOdd = (number%2 == 0) ? "Even": "Odd";
+        System.out.println("The number is " + evenOdd);
+
+        scan.close();
+    }
+}

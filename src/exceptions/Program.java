3f3dcc7449056cd0821fd603026fb3c1d16eb1ad
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-11 20:49:20 -0300

    add stack trace example

--- src/exceptions/Program.java
+++ src/exceptions/Program.java
@@ -6,6 +6,17 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
+        method1();
+    }
+
+    public static void method1() {
+        System.out.println("~~~ METHOD 1 START ~~~");
+        method2();
+        System.out.println("~~~ METHOD 1 END ~~~");
+    }
+
+    public static void method2() {
+        System.out.println("~~~ METHOD 2 START ~~~");
         Scanner scan = new Scanner(System.in);
 
         try {
@@ -14,10 +25,13 @@ public class Program {
             System.out.println(names[position]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position.");
+            e.printStackTrace();
+            scan.next();
         } catch (InputMismatchException e) {
             System.out.println("Wrong input type.");
         }
 
         scan.close();
+        System.out.println("~~~ METHOD 2 END ~~~");
     }
 }

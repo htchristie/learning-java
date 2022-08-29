d5d40a393f34ad6be6b455483b08d4bc38571cab
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-28 23:44:19 -0300

    map examples

---
+++ src/setAndMap/mapExamples/exampleOne/application/Program.java
@@ -0,0 +1,33 @@
+package setAndMap.mapExamples.exampleOne.application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+    public static void main(String[] args) {
+
+        // map is a collection of key value pairs Map<K,V>
+
+        Map<String, String> cookies = new TreeMap<>();
+        // sorted and ascending order
+
+        cookies.put("username", "maria");
+        cookies.put("email", "maria@gmail.com");
+        cookies.put("phone", "99771122");
+
+        cookies.remove("email");
+
+        cookies.put("phone", "99771133");
+
+        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println("Phone number: " + cookies.get("phone"));
+        System.out.println("Email: " + cookies.get("email"));
+        System.out.println("Size: " + cookies.size());
+        System.out.println();
+        System.out.println("ALL COOKIES:");
+
+        for (String key : cookies.keySet()) {
+            System.out.println(key + ": " + cookies.get(key));
+        }
+    }
+}
